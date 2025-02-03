af1e7b5a60f3a1ea4d60d160dd06bdc57146cca2
Jorge Martin <jorgeestudios.martin@gmail.com>
2025-02-03 19:43:05 +0100

    Changed textcontainer.php and css to adjust height of the component at 90vh

--- components/textcontainer/textcontainer.php
+++ components/textcontainer/textcontainer.php
@@ -1,20 +1,20 @@
 <link rel="stylesheet" href="/components/textcontainer/textcontainer.css">
 
-<div class="container-fluid text-center">    
+<div class="container-fluid text-center h-auto">    
   <div class="row content">
-    <div class="col-sm-2 sidenav">
+    <div class="col-sm-2 sidenav h-auto">
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
     </div>
-    <div class="col-sm-8 text-left"> 
+    <div class="col-sm-8 text-left h-auto"> 
       <h1>Welcome</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <hr>
       <h3>Test</h3>
       <p>Lorem ipsum...</p>
     </div>
-    <div class="col-sm-2 sidenav">
+    <div class="col-sm-2 sidenav h-auto">
       <div class="well">
         <p>ADS</p>
       </div>
